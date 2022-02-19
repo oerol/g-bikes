a090b0cc310cb0b91b004254ecf42ee374181075
oerol <erolxerolx@gmail.com>
2022-02-19 13:51:50 +0100

    generic method for editing

--- GreenBikes/Controller/BikeController.cs
+++ GreenBikes/Controller/BikeController.cs
@@ -40,57 +40,35 @@ namespace GreenBikes.Controller
         }
         public void Edit(int index = -1)
         {
-
-            if (bikes.Count == 0)
-            {
-                WriteLine("Keine Einträge zum Bearbeiten verfügbar!");
-                System.Threading.Thread.Sleep(700); // Danach wird der Warnhinweis ausgeblendet
-                new Menu().BikeListMenu();
-                return;
-            }
-            Write("\nBitte wähle einen Index aus und bestätige mit ENTER: ");
-
             if (index == -1)
+
             {
-                index = Utilities.ReadNumberWithMaxValue(ReadLine(), bikes.Count);
-            }
-            Menu.DisplayOptions(bikes[index].ToString() + "\nWas möchtest du ändern?", new string[] { "Hersteller", "Modell", "Leistung (W)", "Kategorie" });
-
-
-            Write("\n");
-            switch (Menu.GetPressedKey())
-            {
-                case 1:
-                    Write("Hersteller: ");
-                    bikes[index].Manufacturer = Utilities.ReadString();
-                    break;
-                case 2:
-                    Write("Modell: ");
-                    bikes[index].Model = Utilities.ReadString();
-                    break;
-                case 3:
-                    Write("Leistung (W): ");
-                    bikes[index].Power = Utilities.ReadByte();
-                    break;
-                case 4:
-                    List<BikeCategory> categories = Utilities.LoadList(new BikeCategory()); // Leeres Objekt für den XMLSerializer ä: in die methode
-                    Utilities.ListItems(categories);
-                    Write("Kategorie?: ");
-                    bikes[index].Category = categories[Utilities.ReadNumberWithMaxValue(ReadLine(), categories.Count)];
-                    break;
-            }
-
-            Utilities.Save(bikes);
-            WriteLine("\n >> " + bikes[index].ToString());
-
-            if (Menu.GetChoice("Änderung wurde vorgenommen. Möchtest du noch etwas ändern?"))
-            {
-                Edit(index); // Wiederholt das Bearbeiten, überspringt aber das Abfragen des Index Ä: lieber oben
+                index = Utilities.EditEntry(bikes, new string[] { "Category" });
             }
             else
             {
-                new Menu().BikeListMenu();
+                index = Utilities.EditEntry(bikes, new string[] { "Category" }, index);
             }
+
+            if (index != -1)
+            {
+                List<BikeCategory> categories = Utilities.LoadList(new BikeCategory()); // Leeres Objekt für den XMLSerializer ä: in die methode
+                Utilities.ListItems(categories);
+                Write("Kategorie?: ");
+                int chosenCategory = Utilities.ReadNumberWithMaxValue(ReadLine(), categories.Count);
+                bikes[index].Category = categories[chosenCategory];
+
+                Utilities.Save(bikes);
+                WriteLine("\n >> " + bikes[index].ToString());
+
+
+                if (Menu.GetChoice("Änderung wurde vorgenommen. Möchtest du noch etwas ändern?"))
+                {
+                    Edit(index);
+                }
+            }
+
+            new Menu().BikeListMenu();
 
         }
 
--- GreenBikes/Controller/Utilities.cs
+++ GreenBikes/Controller/Utilities.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Xml.Serialization;
 using static System.Console;
 using System.Linq;
+using System.Reflection;
 
 namespace GreenBikes.Controller
 {
@@ -220,6 +221,91 @@ namespace GreenBikes.Controller
                     property.SetValue(model, ReadUint());
                 }
             }
+        }
+
+        public static void SetPropertyValue<T>(T model, PropertyInfo property, string[] ignore = null) where T : IModel
+        {
+            if (ignore != null)
+            {
+                if (ignore.Contains(property.Name))
+                {
+                    return;
+                }
+            }
+
+
+            if (property.PropertyType == typeof(string))
+            {
+                property.SetValue(model, ReadString());
+            }
+            else if (property.PropertyType == typeof(byte))
+            {
+                property.SetValue(model, ReadByte());
+            }
+            else if (property.PropertyType == typeof(float))
+            {
+                property.SetValue(model, ReadFloat());
+            }
+            else if (property.PropertyType == typeof(uint))
+            {
+                property.SetValue(model, ReadUint());
+            }
+        }
+        public static int EditEntry<T>(List<T> modelList, string[] ignore = null, int index = -1) where T : IModel
+        {
+            string noEntriesToEdit = "Keine Einträge zum Bearbeiten verfügbar!";
+            string askForIndex = "\nWas möchtest du ändern?";
+            if (modelList.Count == 0)
+            {
+                WriteLine(noEntriesToEdit);
+                return -1;
+            }
+
+            if (index == -1) // Fragt Index ab, sofern keiner vorliegt
+            {
+
+                Write("\nBitte wähle einen Index aus und bestätige mit ENTER: ");
+                index = Utilities.ReadNumberWithMaxValue(ReadLine(), modelList.Count);
+            }
+            string[] translatedProperties = GetGermanProperties(modelList[index]);
+            Menu.DisplayOptions(modelList[index].ToString() + askForIndex, translatedProperties);
+
+            uint pressedKey = Menu.GetPressedKey() - 1;
+
+
+
+            var chosenProperty = modelList[index].GetType().GetProperties()[pressedKey];
+
+            if (ignore.Contains(chosenProperty.Name))
+            {
+                return index;
+            }
+
+            string spacer = ": ";
+            Write(translatedProperties[pressedKey] + spacer);
+            SetPropertyValue(modelList[index], chosenProperty, ignore);
+
+            Utilities.Save(modelList);
+            WriteLine("\n >> " + modelList[index].ToString());
+
+            if (Menu.GetChoice("Änderung wurde vorgenommen. Möchtest du noch etwas ändern?"))
+            {
+                EditEntry(modelList, ignore, index);
+            }
+            return -1;
+        }
+        private static string[] GetGermanProperties<T>(T model) where T : IModel
+        {
+            var properties = model.GetType().GetProperties();
+
+            string[] translatedProperties = new string[0]; // Array statt Liste, weil das Array nach Erstellung nicht bearbeitet wird
+
+            foreach (var property in properties)
+            {
+                string translated = model.ToGerman(property.Name);
+                translatedProperties = translatedProperties.Append(translated).ToArray();
+            }
+            return translatedProperties;
         }
     }
 }
